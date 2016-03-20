f6a381a004ddd803d46b7d1c725a5356d3191fe5
Demurgos <demurgos.net@gmail.com>
2016-03-20 15:16:29 +0100

    Add options to Type interfaces

--- src/core/interfaces/type.ts
+++ src/core/interfaces/type.ts
@@ -4,11 +4,11 @@ export interface TypeAsync<T, D> {
   isSync: boolean;
   name: string;
 
-  read(format: string, val: any): Promise<T>;
-  write(format: string, val: T): Promise<any>;
-  test(val: any): Promise<Error>;
-  normalize(val: any): Promise<T>;
-  equals(val1: T, val2: T): Promise<boolean>;
+  read(format: string, val: any, options?: any): Promise<T>;
+  write(format: string, val: T, options?: any): Promise<any>;
+  test(val: any, options?: any): Promise<Error>;
+  normalize(val: any, options?: any): Promise<T>;
+  equals(val1: T, val2: T, options?: any): Promise<boolean>;
   clone(val: T): Promise<T>;
   diff(oldVal: T, newVal: T): Promise<D>;
   patch(oldVal: T, diff: D): Promise<T>;
@@ -19,29 +19,29 @@ export interface TypeSync<T, D> {
   isSync: boolean;
   name: string;
 
-  readSync(format: string, val: any): T;
-  writeSync(format: string, val: T): any;
-  testSync(val: any): Error;
-  normalizeSync(val: any): T;
-  equalsSync(val1: T, val2: T): boolean;
-  cloneSync(val: T): T;
-  diffSync(oldVal: T, newVal: T): D;
-  patchSync(oldVal: T, diff: D): T;
-  revertSync(newVal: T, diff: D): T;
+  readSync(format: string, val: any, options?: any): T;
+  writeSync(format: string, val: T, options?: any): any;
+  testSync(val: any, options?: any): Error;
+  normalizeSync(val: any, options?: any): T;
+  equalsSync(val1: T, val2: T, options?: any): boolean;
+  cloneSync(val: T, options?: any): T;
+  diffSync(oldVal: T, newVal: T, options?: any): D;
+  patchSync(oldVal: T, diff: D, options?: any): T;
+  revertSync(newVal: T, diff: D, options?: any): T;
 }
 
 export interface Type<T, D> extends TypeAsync<T, D> {
   isSync: boolean;
 
-  readSync?(format: string, val: any): T;
-  writeSync?(format: string, val: T): any;
-  testSync?(val: any): Error;
-  normalizeSync?(val: any): T;
-  equalsSync?(val1: T, val2: T): boolean;
-  cloneSync?(val: T): T;
-  diffSync?(oldVal: T, newVal: T): D;
-  patchSync?(oldVal: T, diff: D): T;
-  revertSync?(newVal: T, diff: D): T;
+  readSync?(format: string, val: any, options?: any): T;
+  writeSync?(format: string, val: T, options?: any): any;
+  testSync?(val: any, options?: any): Error;
+  normalizeSync?(val: any, options?: any): T;
+  equalsSync?(val1: T, val2: T, options?: any): boolean;
+  cloneSync?(val: T, options?: any): T;
+  diffSync?(oldVal: T, newVal: T, options?: any): D;
+  patchSync?(oldVal: T, diff: D, options?: any): T;
+  revertSync?(newVal: T, diff: D, options?: any): T;
 }
 
 export interface StaticTypeSync<T, D> {
@@ -55,13 +55,13 @@ export interface StaticType<T, D> {
 }
 
 export interface CollectionTypeAsync<T, D> extends TypeAsync<T, D> {
-  reflect(visitor: (value?: T, key?: any, parent?: CollectionType<any, any>) => any): Promise<any>;
+  reflect(visitor: (value?: T, key?: any, parent?: CollectionType<any, any>) => any, options?: any): Promise<any>;
 }
 
 export interface CollectionTypeSync<T, D> extends TypeSync<T, D> {
-  reflectSync(visitor: (value?: T, key?: any, parent?: CollectionType<any, any>) => any): any;
+  reflectSync(visitor: (value?: T, key?: any, parent?: CollectionType<any, any>) => any, options?: any): any;
 }
 
 export interface CollectionType<T, D> extends CollectionTypeAsync<T, D>, Type<T, D> {
-  reflectSync?(visitor: (value?: T, key?: any, parent?: CollectionType<any, any>) => any): any;
+  reflectSync?(visitor: (value?: T, key?: any, parent?: CollectionType<any, any>) => any, options?: any): any;
 }
